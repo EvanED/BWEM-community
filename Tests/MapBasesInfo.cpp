4ba26974756dc30d88ce1619c27ec7b3a4876397
Evan Driscoll <evaned@gmail.com>
2018-05-30 22:17:19 -0500

    MapBasesInfo tests pull expected results from JSON now

--- Tests/MapBasesInfo.cpp
+++ Tests/MapBasesInfo.cpp
@@ -1,4 +1,4 @@
-#if 0
+#if 1
 #include "bwem.h"
 #include "mapImpl.h"
 
@@ -6,33 +6,67 @@
 
 #include "gtest/gtest.h"
 
-TEST(MapBasesInfo, MapFindBasesForStartingLocations) {
+class MapBasesInfo : public ::testing::TestWithParam<MapExpectedResults> {
+};
+
+TEST_P(MapBasesInfo, MapFindBasesForStartingLocations) {
 	BWEM::detail::MapImpl map;
 
-  runOnMap("data/maps/(2)Showdown.scx", [&](auto game) {
-    map.Initialize(game);
-  });
+	auto param = GetParam();
+	auto mapName = param.mapName;
+	
+	runOnMap(mapName, [&](auto game) {
+		map.Initialize(game);
+	});
 
 	EXPECT_EQ(true, map.FindBasesForStartingLocations());
 }
 
-TEST(MapBasesInfo, MapStartLocationCount2PlayersMap) {
+template <typename Container, typename Predicate>
+size_t count_if_c(Container const & c, Predicate pred)
+{
+	return std::count_if(std::begin(c), std::end(c), pred);
+}
+
+TEST_P(MapBasesInfo, MapStartLocationCount2PlayersMap) {
 	BWEM::detail::MapImpl map;
 
-  runOnMap("data/maps/(2)Showdown.scx", [&](auto game) {
+	auto param = GetParam();
+	auto mapName = param.mapName;
+	int expectedNumberStarts = count_if_c(
+		param.json["bases"],
+		[](auto const & base_json) {
+		    return base_json["starting_location"].get<bool>();
+	});
+
+  runOnMap(mapName, [&](auto game) {
     map.Initialize(game);
   });
 
+    EXPECT_EQ(expectedNumberStarts, map.StartingLocations().size());
 	EXPECT_EQ(2, map.StartingLocations().size());
 }
 
-TEST(MapBasesInfo, MapBaseCount) {
+TEST_P(MapBasesInfo, MapBaseCount) {
 	BWEM::detail::MapImpl map;
 
-  runOnMap("data/maps/(2)Showdown.scx", [&](auto game) {
+	auto param = GetParam();
+	auto mapName = param.mapName;
+	int expectedBases = param.json["bases"].size();
+
+  runOnMap(mapName, [&](auto game) {
     map.Initialize(game);
   });
 	
+	EXPECT_EQ(expectedBases, map.BaseCount());
 	EXPECT_EQ(8, map.BaseCount());
 }
+
+INSTANTIATE_TEST_CASE_P(
+	BasicCheck,
+	MapBasesInfo,
+	::testing::ValuesIn(getTestData({
+		"data/maps/(2)Showdown.scx"
+		})));
+
 #endif
